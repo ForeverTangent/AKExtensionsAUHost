d566f674a49e0a9debe0086af8793a796085dd1f
Stan Rosenbaum <ForeverTangent@gmail.com>
2020-02-05 19:40:41 -0500

    Minor reorganization.

--- AKExtensionsAUHostV2/BasicSynth1Framework/UI/BasicSynth1AudioUnitViewController.swift
+++ AKExtensionsAUHostV2/BasicSynth1Framework/UI/BasicSynth1AudioUnitViewController.swift
@@ -22,3 +22,21 @@ class BasicSynth1AudioUnitViewController: AUViewController {
 	}
 
 }
+
+
+/*
+Abstract:
+AUv3FilterDemoViewController is the app extension's principal class, responsible for creating both the audio unit and its view.
+*/
+
+//import CoreAudioKit
+//import BasicSynth1Framework
+
+extension BasicSynth1AudioUnitViewController: AUAudioUnitFactory {
+
+	public func createAudioUnit(with componentDescription: AudioComponentDescription) throws -> AUAudioUnit {
+		audioUnit = try BasicSynth1AudioUnit(componentDescription: componentDescription, options: [])
+		return audioUnit!
+	}
+
+}
