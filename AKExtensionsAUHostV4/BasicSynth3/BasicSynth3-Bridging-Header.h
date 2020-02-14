7c53787e8fe31aaf501a9cc95f9534aa393062ff
Stan Rosenbaum <ForeverTangent@gmail.com>
2020-02-14 16:30:45 -0500

    Removed V4 for a moment

--- AKExtensionsAUHostV4/BasicSynth3/BasicSynth3-Bridging-Header.h
+++
@@ -1,9 +0,0 @@
-//
-//  BasicSynth3-Bridging-Header.h
-//  BasicSynth3
-//
-//  Created by Stanley Rosenbaum on 2/13/20.
-//  Copyright © 2020 STAQUE. All rights reserved.
-//
-
-#import "BasicSynth3DSPKernelAdapter.h"
